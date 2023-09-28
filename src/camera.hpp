a3e2d663763e167ceeed3b46747e5576970d7be8
Pratik <vakilpratik@gmail.com>
2023-09-28 10:08:36 +0530

    9.2 limiting number of child rays

--- src/camera.hpp
+++ src/camera.hpp
@@ -11,6 +11,7 @@ public:
     double aspect_ratio = 1.;
     int image_width = 100;
     int samples_per_pixel = 10;
+    int max_depth = 10;
 
     void render(const hittable &world)
     {
@@ -29,7 +30,7 @@ public:
                 for (int sample = 0; sample < samples_per_pixel; sample++)
                 {
                     auto r = get_ray(i, j);
-                    pixel_color += ray_color(r, world);
+                    pixel_color += ray_color(r, max_depth, world);
                 }
 
                 write_color(std::cout, pixel_color, samples_per_pixel);
@@ -80,13 +81,17 @@ private:
         return (px * pixel_delta_u) + (py * pixel_delta_v);
     }
 
-    color ray_color(const ray &r, const hittable &world) const
+    color ray_color(const ray &r, int depth, const hittable &world) const
     {
+        if (depth <= 0)
+        {
+            return color(0., 0., 0.);
+        }
         hit_record rec;
         if (world.hit(r, interval(0, infinity), rec))
         {
             vec3 direction = random_on_hemisphere(rec.normal);
-            return 0.5 * ray_color(ray(rec.p, direction), world); // rec.normal + color(1., 1., 1.));
+            return 0.5 * ray_color(ray(rec.p, direction), depth - 1, world); // rec.normal + color(1., 1., 1.));
         }
         vec3 unit_direction = unit_vector(r.direction());
         auto a = 0.5 * (unit_direction.y() + 1.0);
